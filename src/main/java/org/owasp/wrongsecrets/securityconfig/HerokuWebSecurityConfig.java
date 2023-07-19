c18a894b622703c052421ff507f46ef404bca784
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2023-07-19 17:29:06 +0200

    SS6.0 test

--- src/main/java/org/owasp/wrongsecrets/securityconfig/HerokuWebSecurityConfig.java
+++ src/main/java/org/owasp/wrongsecrets/securityconfig/HerokuWebSecurityConfig.java
@@ -3,8 +3,8 @@ package org.owasp.wrongsecrets.securityconfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 /** Used to implement https redirect for our Heroku-hosted workload. */
 @Configuration
@@ -12,14 +12,14 @@ public class HerokuWebSecurityConfig {
 
   @Bean
   @Order(1)
-  public SecurityFilterChain configureHerokuWebSecurity(HttpSecurity http) throws Exception {
-    http.requiresChannel()
-        .requestMatchers(
-            r ->
-                r.getRequestURL().toString().contains("heroku")
-                    && (r.getHeader("x-forwarded-proto") != null
-                        || r.getHeader("X-Forwarded-Proto") != null))
-        .requiresSecure();
-    return http.build();
+  public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.redirectToHttps(
+            redirect ->
+                redirect.httpsRedirectWhen(
+                    r ->
+                       r.getRequest().getURI().toString().contains("heroku")
+                            && (r.getRequest().getHeaders().containsKey("X-Forwarded-Proto")
+                                || r.getRequest().getHeaders().containsKey("x-forwarded-proto"))));
+        return http.build();
   }
 }
